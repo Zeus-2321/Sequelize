dc21424e1f53ae963cdbdeba562f0cd2b4e88f36
Dev <devpratap2321@gmail.com>
2024-11-22 14:39:22 +0530

    Added Auth and encryption

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+require('dotenv').config({path: `${process.cwd()}/.env`})
 const express = require('express');
+
 const app = express();
 
+app.use(express.json());
+
 const authRouter = require('./routes/authRoute')
-app.get('/', (req, res)=> {
+
+app.get('/', (req, res) => {
     res.status(200).json({
         status:'success',
         message:'APIs are working!'
@@ -10,7 +15,15 @@ app.get('/', (req, res)=> {
 })
 
 app.use('/api/auth', authRouter);
+app.use('*', (req, res) => {
+    res.status(404).json({
+        status:'fail',
+        message:'Route Not Defined'
+    })
+})
 
-app.listen(3000, ()=>{
-    console.log("Server up and running on port 3000")
+const PORT = process.env.APP_PORT || 4000;
+
+app.listen(process.env.APP_PORT, ()=>{
+    console.log(`Server up and running on port ${PORT}`)
 })
--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,33 @@
-const signUp = (req, res, next) => {
-    res.json({
-        status:'success',
-        message:'SignUp route working'
+const user = require("../db/models/user");
+
+const signUp = async (req, res, next) => {
+    const body = req.body;
+
+    if(!['1', '2'].includes(body.userType)) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Invalid User Type'
+        })
+    }
+
+    const newUser = await user.create({
+        userType: body.userType,
+        firstName: body.firstName,
+        lastName: body.lastName,
+        email: body.email,
+        password: body.password,
+        confirmPassword: body.confirmPassword
+    })
+
+    if(!newUser) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Failed to create the user'
+        })
+    }
+    return res.status(201).json({
+        status: 'success',
+        data: newUser
     })
 }
 
